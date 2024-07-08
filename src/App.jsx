d1d6197d9615a18ecd8076e52a787c10f45fee7f
zachdev1 <zmsoftdev@gmail.com>
2024-07-08 17:39:03 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>NBA Search</h1>
+    <input type="text" placeholder='player name'/>
+    <button>X</button>
+    </>
+  )
+}
+
+export default App;
