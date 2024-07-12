28ce2449950d6a7f1a348d8ba1b20e6577649169
zachdev1 <zmsoftdev@gmail.com>
2024-07-12 14:49:29 -0400

    added some things

--- server/main.py
+++ server/main.py
@@ -13,22 +13,38 @@ cors = CORS(app, origins='*')
 def getPlayer():
     player_name = request.args.get('name')
     if player_name:
+        
         nba_players = players.get_players()
-        playerId = None
+
+        player_id = None
         for player in nba_players:
             if player['full_name'] == player_name:
-                playerId = player['id']
+                player_id = player['id']
                 break
-        if playerId:
-            return jsonify({'playerId': playerId})
-        else:
+            
+        if player_id is None:
             return jsonify({'error': 'Player not found'})
         
-        
+        # Get career stats
+        career_stats = playercareerstats.PlayerCareerStats(player_id=player_id)
+        df = career_stats.get_data_frames()[0]
+
+        # Select specific columns and calculate per-game averages
+        df1 = df[['SEASON_ID', 'TEAM_ABBREVIATION', 'GP', 'PTS', 'REB', 'AST', 'BLK', 'STL']]
+        averages = pd.DataFrame()
+        averages['SEASON_ID'] = df1['SEASON_ID']
+        averages['TEAM_ABBREVIATION'] = df1['TEAM_ABBREVIATION']
+        averages['PPG'] = (df1['PTS'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['RPG'] = (df1['REB'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['APG'] = (df1['AST'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['BPG'] = (df1['BLK'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['SPG'] = (df1['STL'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+
+        # Convert DataFrame to JSON
+        averages_json = averages.to_dict(orient='records')
+        return jsonify(averages_json)
     else:
         return jsonify({'error': 'Player name parameter is missing'})
     
-    
-
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,81 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
+import './App.css';
 
-function App(){
+const App = () => {
   const [playerName, setPlayerName] = useState('');
+  const [playerStats, setPlayerStats] = useState('');
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.get(`http://localhost:8080/api/players?name=${playerName}`);
-      console.log(response.data); 
+      setPlayerStats(response.data); 
     } catch (error) {
       console.error('Error fetching player data:', error);
     }
   };
 
-  const handleChange = (e) => {
-    setPlayerName(e.target.value);
-  };
-
   return (
-    <div className="App">
-      <h1>NBA Search</h1>
+    <div className='App'>
+      <header>
+        <img src="assets/icons8-basketball-96.png" alt='basketball' />
+        <h1>NBA Search</h1>
+      </header>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="playerName">Player Name</label>
         <input 
           type="text" 
           id="playerName"
           placeholder='Enter Player Name'
           value={playerName}
-          onChange={handleChange}
+          onChange={(e) => setPlayerName(e.target.value)}
         />
+        <br />
         <button type='submit'>Search</button>
       </form>
+
+      <div className='Avg'>
+        <h1>Player Season Averages</h1>
+        {playerStats.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Team</th>
+              <th>PPG</th>
+              <th>RPG</th>
+              <th>APG</th>
+              <th>BPG</th>
+              <th>SPG</th>
+            </tr>
+          </thead>
+          <tbody>
+          {playerStats.map((stat, index) => (
+                <tr key={index}>
+                  <td>{stat.SEASON_ID}</td>
+                  <td>{stat.TEAM_ABBREVIATION}</td>
+                  <td>{stat.PPG}</td>
+                  <td>{stat.RPG}</td>
+                  <td>{stat.APG}</td>
+                  <td>{stat.BPG}</td>
+                  <td>{stat.SPG}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+
+      <footer>
+        <p>Made by: Zachary Mohamed</p>
+        <p>
+          <a target="_blank" href="https://github.com/zachdev1/nba-search">GitHub Repo</a>
+        </p>
+        <a target="_blank" href="https://icons8.com/icon/khlCdbEXD0Sp/basketball">Basketball</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+      </footer>
     </div>
   );
 }
