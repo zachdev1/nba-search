fefeca6c8af62303ad3c6bb9b4ba0d381b156d7c
zachdev1 <zmsoftdev@gmail.com>
2024-08-15 12:29:08 -0400

    trying railway

---
+++ api/main.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+import pandas as pd
+import numpy as np
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
+
+app = Flask(__name__)
+cors = CORS(app, origins=["*"])
+
+@app.route("/")
+def health_check():
+    return "OK", 200
+
+@app.route("/api/players", methods=['GET'])
+def getPlayer():
+    player_name = request.args.get('name')
+    if player_name:
+        
+        nba_players = players.get_players()
+
+        player_id = None
+        for player in nba_players:
+            if player['full_name'] == player_name:
+                player_id = player['id']
+                break
+            
+        if player_id is None:
+            return jsonify({'error': 'Player not found'}), 404
+        
+        # Get career stats
+        career_stats = playercareerstats.PlayerCareerStats(player_id=player_id)
+        df = career_stats.get_data_frames()[0]
+
+        # Select specific columns and calculate per-game averages
+        df1 = df[['SEASON_ID', 'TEAM_ABBREVIATION', 'GP', 'PTS', 'REB', 'AST', 'BLK', 'STL']]
+        averages = pd.DataFrame()
+        averages['SEASON_ID'] = df1['SEASON_ID']
+        averages['TEAM_ABBREVIATION'] = df1['TEAM_ABBREVIATION']
+        averages['PPG'] = (df1['PTS'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['RPG'] = (df1['REB'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['APG'] = (df1['AST'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['BPG'] = (df1['BLK'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+        averages['SPG'] = (df1['STL'] / df1['GP']).apply(lambda x: f"{x:.2f}")
+
+        # Convert DataFrame to JSON
+        averages_json = averages.to_dict(orient='records')
+        return jsonify(averages_json)
+    else:
+        return jsonify({'error': 'Player name parameter is missing'}), 400
+    
+if __name__ == '__main__':
+    app.run(debug=True)
