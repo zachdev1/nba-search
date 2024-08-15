e2727a0bdc32771def1db904bb6283afc00ce2ba
zachdev1 <zmsoftdev@gmail.com>
2024-08-15 12:38:13 -0400

    fixed url

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
   const [playerName, setPlayerName] = useState('');
   const [playerStats, setPlayerStats] = useState('');
 
-  // const URL = process.env.URL 
-const URL = "http://localhost:5000"
+const URL = process.env.URL
+// const URL = "http://localhost:5000"
 
   const handleSubmit = async (e) => {
     e.preventDefault();
